72211a446e7904022de0fcde442333258a39e515
BIGBEN <decagon@macs-MacBook-Air.local>
2023-09-09 11:43:15 +0100

    new1

--- src/contoller/profile.ts
+++ src/contoller/profile.ts
@@ -8,38 +8,33 @@ export const getProfileDetails = (
   const { slack_name } = req.query;
   const { track } = req.query;
 
-  // Function to pad single digits with leading zeros
-  function padZero(num: number) {
-    return num < 10 ? `0${num}` : num;
-  }
-
   // Get the current UTC time
   const currentUTC = new Date();
-  const utcYear = currentUTC.getUTCFullYear();
-  const utcMonth = padZero(currentUTC.getUTCMonth() + 1); // Months are zero-indexed
-  const utcDay = padZero(currentUTC.getUTCDate());
-  const utcHours = padZero(currentUTC.getUTCHours());
-  const utcMinutes = padZero(currentUTC.getUTCMinutes());
-  const utcSeconds = padZero(currentUTC.getUTCSeconds());
 
-  const currentDateTime = `${utcYear}-${utcMonth}-${utcDay}T${utcHours}:${utcMinutes}:${utcSeconds}Z`;
+  // Validate that the current time is within +/-2 seconds of the desired format
+  const allowedDifferenceInSeconds = 2;
+  const desiredFormat = `${currentUTC.getUTCFullYear()}-${padZero(
+    currentUTC.getUTCMonth() + 1
+  )}-${padZero(currentUTC.getUTCDate())}T${padZero(
+    currentUTC.getUTCHours()
+  )}:${padZero(currentUTC.getUTCMinutes())}:${padZero(
+    currentUTC.getUTCSeconds()
+  )}Z`;
+  const currentTimeInDesiredFormat = currentUTC.toISOString();
+  const timeDifferenceInSeconds = Math.abs(
+    (new Date(desiredFormat).getTime() - currentUTC.getTime()) / 1000
+  );
 
-  // Get the current day of the week
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  const currentDayOfWeek = daysOfWeek[currentUTC.getUTCDay()];
+  if (timeDifferenceInSeconds > allowedDifferenceInSeconds) {
+    // The current time is outside the allowed range
+    return res
+      .status(500)
+      .json({ error: "Current time is outside the allowed range" });
+  }
 
   const slackProfile = {
     slack_name: slack_name,
-    current_day: currentDayOfWeek,
-    utc_time: currentDateTime,
+    utc_time: currentTimeInDesiredFormat,
     track: track,
     github_file_url: `https://github.com/Bigben1200/ZuriProfile/blob/main/src/app.ts`,
     github_repo_url: `https://github.com/Bigben1200/ZuriProfile`,
@@ -47,8 +42,15 @@ export const getProfileDetails = (
   };
 
   try {
-    res.send(slackProfile);
+    const formattedResponse = JSON.stringify(slackProfile, null, 2);
+    res.set("Content-Type", "application/json");
+    res.send(formattedResponse);
   } catch (error) {
     next(error);
   }
 };
+
+// Function to pad single digits with leading zeros
+function padZero(num: number) {
+  return num < 10 ? `0${num}` : num;
+}
